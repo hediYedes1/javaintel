8b251e992b4017c68e97eb9a0e83317ef3333168
hediYedes1 <mohamedhedi.yedes@esprit.tn>
2024-09-29 15:46:51 +0100

    first commit

---
+++ Prosit4/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal lion = new Animal("cats", "lion", 3, true);
+        Animal zebra = new Animal("zebra", "zebra", 3, true);
+        Animal elephant = new Animal("elephant", "elephant", 3, true);
+        Animal giraffe = new Animal("giraffe", "giraffe", 3, true);
+        Animal monkey = new Animal("monkey", "monkey", 3, true);
+        Animal tiger = new Animal("tiger", "tiger", 3, true);
+
+        Zoo myZoo = new Zoo("Belvedere", "Tunis");
+        Zoo myZoo1 = new Zoo("Frigya", "Bouficha");
+
+        myZoo.displayZoo();
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+
+        myZoo.addAnimal(zebra);
+        myZoo.addAnimal(elephant);
+        myZoo.addAnimal(giraffe);
+        myZoo.addAnimal(monkey);
+
+        System.out.println(myZoo);
+        System.out.println("pos: " + myZoo.searchAnimal(lion.name));
+        System.out.println("pos: " + myZoo.searchAnimal(giraffe.name));
+
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(lion); // Tentative d'ajout de l'animal en double
+        myZoo.addAnimal(tiger);
+
+        myZoo.removeAnimal(monkey);
+        System.out.println(myZoo);
+
+        myZoo.isZooFull();
+        System.out.println("Le zoo avec le plus grand nombre d'animaux est: " + Zoo.comparerZoo(myZoo, myZoo1));
+    }
+}
---
+++ Prosit4/src/Zoo.java
@@ -0,0 +1,88 @@
+public class Zoo {
+    public Animal[] animals;
+    public String name;
+    public String city;
+    public final int nbrCages = 25;
+    public int nbrAnimals;
+
+    public Zoo(String name, String city) {
+        this.name = name;
+        this.city = city;
+        this.animals = new Animal[this.nbrCages];
+        this.nbrAnimals = 0;
+    }
+
+    public void displayZoo() {
+        System.out.println("Name: " + this.name);
+        System.out.println("City: " + this.city);
+        System.out.println("Nbr Cages: " + this.nbrCages);
+    }
+
+    public String toString() {
+        String msg = "\nName: " + this.name + "\nCity: " + this.city + "\nNbr Cages: " + this.nbrCages + "\nAnimals:";
+        for (int i = 0; i < this.nbrAnimals; i++) {
+            msg += "\n" + (i + 1) + ". " + this.animals[i].toString();
+        }
+        return msg;
+    }
+
+    public int searchAnimal(String animalName) {
+        for (int i = 0; i < this.nbrAnimals; i++) {
+            if (this.animals[i].name.equals(animalName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean addAnimal(Animal animal) {
+        if (this.nbrAnimals >= this.nbrCages) {
+            System.out.println("No more additions allowed!");
+            return false;
+        } else if (this.searchAnimal(animal.name) != -1) {
+            System.out.println("Animal already exists!");
+            return false;
+        } else {
+            this.animals[this.nbrAnimals] = animal;
+            this.nbrAnimals++;
+            System.out.println("Animal added");
+            return true;
+        }
+    }
+
+    public boolean removeAnimal(Animal animal) {
+        int index = this.searchAnimal(animal.name);
+        if (index == -1) {
+            System.out.println("Animal not found!");
+            return false;
+        } else {
+            for (int i = index; i < this.nbrAnimals - 1; i++) {
+                this.animals[i] = this.animals[i + 1];
+            }
+            this.animals[this.nbrAnimals - 1] = null;
+            this.nbrAnimals--;
+            System.out.println("Animal removed");
+            return true;
+        }
+    }
+
+    public boolean isZooFull() {
+        if (this.nbrAnimals == this.nbrCages) {
+            System.out.println("Zoo is full!");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        if (z1.nbrAnimals > z2.nbrAnimals) {
+            return z1;
+        } else if (z2.nbrAnimals > z1.nbrAnimals) {
+            return z2;
+        } else {
+            System.out.println("Both zoos have the same number of animals.");
+            return null;
+        }
+    }
+}
